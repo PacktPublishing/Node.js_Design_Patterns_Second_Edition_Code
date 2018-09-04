a7c18d0f681aedaa7339eccb63cf1d2ddb1b95d3
Bassem GHoniem <bassem.ghoniem@yahoo.com>
2018-09-04 20:33:22 +0200

    fix issue in Chapter02/10_module_loader (#18)
    * Adding Option {remove, null} to slug call to prevent removing of '.' (the dot) in the downloaded file names
    
    * fix issue in Chapter02/10_module_loader

--- Chapter02/10_module_loader/loader.js
+++ Chapter02/10_module_loader/loader.js
@@ -13,7 +13,7 @@ function loadModule(filename, module, require) {
 }
 
 // We intentionally use var in the next line to avoid "SyntaxError: Identifier 'require' has already been declared"
-const require = (moduleName) => {
+var require = (moduleName) => {
   console.log(`Require invoked for module: ${moduleName}`);
   const id = require.resolve(moduleName);      //[1]
   if(require.cache[id]) {           //[2]
